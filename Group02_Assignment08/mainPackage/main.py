19937b91e03822f32a955055f871c1ef5f1a99c0
harrints <harrints@vdi-LCB-IC-014.ad.uc.edu>
2024-03-26 11:33:49 -0400

    First Commit

--- Group02_Assignment08/mainPackage/main.py
+++ Group02_Assignment08/mainPackage/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #main.py
 if __name__ == "__main__":
     from dataClassPackage.dataClass import Data
@@ -23,29 +22,4 @@ if __name__ == "__main__":
     for row in myCursor:
         print(row[1], end=", ")
     
-    
-=======
-#Main.py
-if __name__ == "__main__":
-    from dataClassPackage.dataClass import Data
-    
-    #Instantiate an object of type Data
-    myData = Data()
-    #Invoke the connect method and store what it returns in another variable 
-    myCursor = myData.Connect("GroceryStoreSimulator")   
-    
-    
-    myCursor.execute(
-        '''
-        SELECT TOP 10 tName.NameID, tName.Name, tProduct.ProductID, tTransactionDetail.QtyOfProduct 
-        FROM tName 
-        INNER JOIN tProduct ON tName.NameID = tProduct.NameID 
-        INNER JOIN tTransactionDetail ON tProduct.ProductID = tTransactionDetail.ProductID 
-        ORDER BY tTransactionDetail.QtyOfProduct DESC
-    '''
-    )
-    print ("The Top 10 Items sold are...")
-    for row in myCursor:
-        print (row[1]); # Second column
-        
->>>>>>> refs/remotes/origin/master
+   
