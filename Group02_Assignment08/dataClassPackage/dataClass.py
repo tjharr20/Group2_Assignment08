8c34c8ba643991b1d0fdf291bc54d834207fa871
harrints <harrints@vdi-LCB-IC-014.ad.uc.edu>
2024-03-26 11:56:28 -0400

    first commit

--- Group02_Assignment08/dataClassPackage/dataClass.py
+++ Group02_Assignment08/dataClassPackage/dataClass.py
@@ -1,5 +1,14 @@
-
 #dataClass.py
+
+# Name: TJ Harrington, Ben Klein, Ryan Wilkins
+# email: harrints@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 03/28/24
+# Course/Section: IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This is a group assignment and it has us use SQL and Python together to write a sentence about grocery store.
+# Brief Description of what this module does: This module logs us into the SQL Server housing all of the data we use for the assignment
+
 import pyodbc
 class Data: 
     
--- Group02_Assignment08/mainPackage/main.py
+++ Group02_Assignment08/mainPackage/main.py
@@ -1,4 +1,13 @@
 #main.py
+
+# Name: TJ Harrington, Ben Klein, Ryan Wilkins
+# email: harrints@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 03/28/24
+# Course/Section: IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This is a group assignment and it has us use SQL and Python together to write a sentence about grocery store.
+# Brief Description of what this module does: This module is the main module and it is where we run all of our code. This fetches the data from the Grocery Store simulator and prints our findings into 1 sentence
 if __name__ == "__main__":
     from dataClassPackage.dataClass import Data
     
